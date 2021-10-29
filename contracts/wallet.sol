5b0b10b0749dff67f96911f7a842f8f823a62a1f
Oliver-Ryall <oliryall0404@gmail.com>
2021-10-29 19:03:58 +0100

    Add documentation to wallet contract

--- contracts/wallet.sol
+++ contracts/wallet.sol
@@ -4,37 +4,63 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title Wallet
+ * @dev Wallet contract enables dex users 
+ * to store their cryptocurrencies
+ */
 contract Wallet is Ownable {
     using SafeMath for uint256;
-
+    
     modifier tokenExists(bytes32 ticker){
-        require(tokenMapping[ticker].tokenAddress != address(0), "token does not exist");
-        _;
-    }
-
-    event Deposited(uint amount, bytes32 ticker);
+            require(tokenMapping[ticker].tokenAddress != address(0), "token does not exist");
+            _;
+        }
 
     struct Token {
-        bytes32 ticker;
-        address tokenAddress;
-    }
+            bytes32 ticker;
+            address tokenAddress;
+        }
 
+    // Mapping from ticker to the Token   
     mapping(bytes32 => Token) public tokenMapping;
+    
+    // Array storing all tickers(list of tokens)
     bytes32[] public tokenList;
 
+    // Mapping from address -> ticker -> amount(quantity of tokens in balance)
     mapping(address=> mapping(bytes32 => uint256)) public balances;
 
+    // Event emited when tokens are deposited
+    event Deposited(uint amount, bytes32 ticker);
+
+    /**
+     * @dev Allows users to add the capability of 
+     * storing a paticular Token on the wallet 
+     * @param ticker Represents the symbol(ticker) of the Token
+     * @param tokenAddress Represents the address for the Token being added
+     */
     function addToken(bytes32 ticker, address tokenAddress) external onlyOwner {
         tokenMapping[ticker] = Token(ticker, tokenAddress);
         tokenList.push(ticker);
     }
 
+    /**
+     * @dev Allows users to deposit tokens onto their wallet
+     * @param amount Represents the amount of tokens being deposited
+     * @param ticker Represents the symbol(ticker) of the token being deposited
+     */
     function deposit(uint amount, bytes32 ticker) payable external tokenExists(ticker) {
         emit Deposited(amount, ticker);
         IERC20(tokenMapping[ticker].tokenAddress).transferFrom(msg.sender, address(this), amount);
         balances[msg.sender][ticker] = balances[msg.sender][ticker].add(amount);
     }
 
+    /**
+     * @dev Allows users to withraw tokens from wallet 
+     * @param amount Represents the amount of tokens being withdrawn
+     * @param ticker Represents the symbol(ticker) of the tokens being withdrawn
+     */
     function withdraw(uint amount, bytes32 ticker) external tokenExists(ticker) {
         require(balances[msg.sender][ticker] >= amount, "balance not sufficient");
         
